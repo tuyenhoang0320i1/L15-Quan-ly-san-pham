6152a59df87d6769cda65a553a5cb913c5571ea5
tuyenhoang0320i1 <63198356+tuyenhoang0320i1@users.noreply.github.com>
2020-04-21 23:39:01 +0700

    file tam thoi

---
+++ ProductManager.js
@@ -0,0 +1,36 @@
+let productList = new Array("Sony Xperia", "Samsung Galaxy", "Nokia 6", "Xiaomi Mi 5s Plus", "Apple Iphone 8 Plus");
+function displayTableFromArray(productList) {
+    let str ="<table>"
+    + "<caption>Display all product: </caption>"
+    + "<tr>"
+    + "<th width='30%'>&ensp;Product name</th>"
+    + "<th>&ensp;</th>"
+    + "<th>&ensp;</th>"
+    + "</tr>";
+    for (let i = 0; i < productList.length; i++) {
+        str += `<tr><td>${productList[i]}</td>
+        <td><button onclick="editProduct(${i}">Edit</button> </td>
+        <td><button onclick="editProduct(${i}">Delete</button> </td></tr>`;
+    }
+    str += "</table>";
+    document.getElementById('displayTable').innerHTML = str;
+}
+
+function addProduct() {
+    let product = document.getElementById('newProduct').value;
+    if (product !== '') {
+        productList.push(product);
+        displayTableFromArray(productList);
+    }
+}
+function editProduct(index) {
+    productList[index] = prompt("Input new name of product");
+    displayTableFromArray(productList);
+}
+function deleteProduct(index) {
+    productList.splice(index, 1);
+    displayTableFromArray(productList);
+
+}
+displayTableFromArray(productList);
+deleteProduct();
